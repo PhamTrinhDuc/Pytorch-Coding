b90fa97e342b48ef765a5cf8a78f177cfe96f464
PhamTrinhDuc <duc78240@gmail.com>
2025-01-22 00:08:06 +0700

    add router api

---
+++ Engineering/FastAPI/router/catdog_route.py
@@ -0,0 +1,26 @@
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).parent))
+
+from fastapi import FastAPI, File, UploadFile
+from fastapi import APIRouter
+from schemas.catdog_schema import CatDogResponse
+from model.catdot_predictor import CatDogPredictor
+from config import ModelConfig
+
+config = ModelConfig()
+
+predictor = CatDogPredictor(
+    model_name=config.MODEL_NAME,
+    model_path=config.MODEL_WEIGHT,
+    device=config.DEVICE
+)
+
+router = APIRouter()
+
+@router.post("/predict")
+async def predict(file_upload: UploadFile = File(...)):
+    response = await predictor.predict(image_path=file_upload.file)
+    
+    return CatDogResponse(**response)
