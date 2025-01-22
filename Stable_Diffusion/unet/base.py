1f485b895ed91a58a0fb703bdf2d7ca5e25b95ca
PhamTrinhDuc <duc78240@gmail.com>
2025-01-22 17:58:43 +0700

    unet from scratch

--- Stable_Diffusion/unet/base.py
+++ Stable_Diffusion/unet/base.py
@@ -87,25 +87,32 @@ class Unet(nn.Module):
 
         self.encoders = nn.ModuleList([
 
-            # [B, in_channels, H/8, W/8] => [B, channels, H/8, W/8]
+            # 1. [B, in_channels, H/8, W/8] => [B, channels, H/8, W/8]
             SwitchSequential(
                 nn.Conv2d(in_channels=in_channels, 
                           out_channels=channels, 
                           kernel_size=3, 
                           padding=1)
             ),
-            # 1. [B, channels, H/8, W/8] => [B, channels, H/8, W/8]
+            # 2. [B, channels, H/8, W/8] => [B, channels, H/8, W/8]
             SwitchSequential(
                 UnetResidualBlock(in_channels=channels, 
                                   out_channels=channels),
                 UnetAttentionBlock(d_model=d_model, 
                                    num_heads=num_heads),
             ),
-            # [B, channels, H/8, W/8] => [B, channels, H/16, W/16]
+            # 3. [B, channels, H/8, W/8] => [B, channels, H/8, W/8]
+            SwitchSequential(
+                UnetResidualBlock(in_channels=channels, 
+                                  out_channels=channels),
+                UnetAttentionBlock(d_model=d_model, 
+                                   num_heads=num_heads),
+            ),
+            # 4. [B, channels, H/8, W/8] => [B, channels, H/16, W/16]
             SwitchSequential(
                 DownSample(channels=channels),
             ),
-            # 2. [B, channels, H/16, W/16] => [B, channels*2, H/16, W/16]
+            # 5. [B, channels, H/16, W/16] => [B, channels*2, H/16, W/16]
             # because channels = d_model * num_heads so when channels increase 2 to then d_model increase 2
             SwitchSequential(
                 UnetResidualBlock(in_channels=channels, 
@@ -113,41 +120,43 @@ class Unet(nn.Module):
                 UnetAttentionBlock(d_model=d_model*2, 
                                    num_heads=num_heads),
             ),
-            # [B, channels, H/16, W/16] => [B, channels*2, H/16, W/16]
+            # 6. [B, channels*2, H/16, W/16] => [B, channels*2, H/16, W/16]
             SwitchSequential(
                 UnetResidualBlock(in_channels=channels*2, 
                                   out_channels=channels*2),
                 UnetAttentionBlock(d_model=d_model*2, 
                                    num_heads=num_heads),
             ),
-            # [B, channels*2, H/16, W/16] => [B, channels*2, H/32, W/32]
+            # 7. [B, channels*2, H/16, W/16] => [B, channels*2, H/32, W/32]
             SwitchSequential(
                 DownSample(channels=channels*2),
             ),
-            # 3. [B, channels*2, H/32, W/32] => [B, channels*4, H/32, W/32]
+            # 8. [B, channels*2, H/32, W/32] => [B, channels*4, H/32, W/32]
             SwitchSequential(
                 UnetResidualBlock(in_channels=channels*2, 
                                   out_channels=channels*4),
                 UnetAttentionBlock(d_model=d_model*4, 
                                    num_heads=num_heads),
             ),
-            # [B, channels*4, H/32, W/32] => [B, channels*4, H/32, W/32]
+            # 9. [B, channels*4, H/32, W/32] => [B, channels*4, H/32, W/32]
             SwitchSequential(
                 UnetResidualBlock(in_channels=channels*4, 
                                   out_channels=channels*4),
                 UnetAttentionBlock(d_model=d_model*4, 
                                    num_heads=num_heads),
             ),
-            # [B, channels*4, H/32, W/32] => [B, channels*4, H/64, W/64]
+            # 10. [B, channels*4, H/32, W/32] => [B, channels*4, H/64, W/64]
             SwitchSequential(
                 DownSample(channels=channels*4),
             ),
-            # 4. pass through 2 Residuals block 
+            # 11. pass through 2 Residuals block 
             # [B, channels*4, H/64, W/64] => [B, channels*4, H/64, W/64]
-            SwitchSequential(
-                *[UnetResidualBlock(in_channels=channels*4, 
-                                    out_channels=channels*4)
-                                    for _ in range(2)]
+            SwitchSequential(UnetResidualBlock(in_channels=channels*4, 
+                                               out_channels=channels*4)
+            ),
+            # 12.[B, channels*4, H/64, W/64] => [B, channels*4, H/64, W/64]
+            SwitchSequential(UnetResidualBlock(in_channels=channels*4, 
+                                               out_channels=channels*4)
             ),
         ])
 
@@ -161,23 +170,104 @@ class Unet(nn.Module):
             ) for _ in range(2)],
         ])
 
-        # self.decoders = nn.ModuleList([
-        #     # 4'. [B, channel*4*2, H/64, W/64] => [B, channels*4, H/64, W/64]
-        #     # becauce inpute decoder concat the skip connection of the encoder 
-        #     *[UnetResidualBlock(in_channels=channels*4*2,
-        #                       out_channels=channels*4)],
-        #     Upsample(channels=channels*4*2),
-        #     UnetBlock(in_channels=channels*4*2,
-        #               out_channels=channels*4,
-        #               d_model=d_model*4,
-        #               num_heads=num_heads),
-        #     # [B, channels*4, H/64, W/64] => [B, channels*4, H/32, W/32]
-        #     Upsample(channels=channels*4),
-
-        #     # 3'. [B, channels*4, H/32, W/32] => [B, channels*2, H/16, W/16]
-        #     *[UnetBlock(in_channels=channels*4, 
-        #                 out_channels=channels*2)],
-        # ])
+        """
+        Output of encoder layers: 
+            1: torch.Size([1, 320, 32, 32])
+            2: torch.Size([1, 320, 32, 32])
+            3: torch.Size([1, 320, 32, 32])
+            4: torch.Size([1, 320, 16, 16])
+            5: torch.Size([1, 640, 16, 16])
+            6: torch.Size([1, 640, 16, 16])
+            7: torch.Size([1, 640, 8, 8])
+            8: torch.Size([1, 1280, 8, 8])
+            9: torch.Size([1, 1280, 8, 8])
+            10: torch.Size([1, 1280, 4, 4])
+            11: torch.Size([1, 1280, 4, 4])
+            12: torch.Size([1, 1280, 4, 4])
+        """
+        self.decoders = nn.ModuleList([
+        
+            # 12'. [B, channel*4*2, H/64, W/64] => [B, channels*4, H/64, W/64]
+            # becauce inpute decoder concat the skip connection of the encoder 
+            SwitchSequential(UnetResidualBlock(in_channels=channels*4*2,
+                                               out_channels=channels*4) 
+            ),
+            # 11'. [B, channel*4*2, H/64, W/64] => [B, channels*4, H/64, W/64]
+             SwitchSequential(UnetResidualBlock(in_channels=channels*4*2,
+                                               out_channels=channels*4),
+            ),
+            # 10'. [B, channels*4*2, H/64, W/64] => [B, channels*4, H/32, W/32]
+             SwitchSequential(
+                UnetResidualBlock(in_channels=channels*4*2,
+                                  out_channels=channels*4),
+                Upsample(channels=channels*4)
+            ),
+            # 9'. [B, channels*4*2, H/32, W/32] => [B, channels*4, H/32, W/32]
+            SwitchSequential(
+                UnetResidualBlock(in_channels=channels*4*2, 
+                                  out_channels=channels*4),
+                UnetAttentionBlock(d_model=d_model*4,
+                                   num_heads=num_heads),
+            ),
+            # 8'. [B, channels*4*2, H/32, W/32] => [B, channels*4, H/32, W/32]
+            SwitchSequential(
+                UnetResidualBlock(in_channels=channels*4*2, 
+                                  out_channels=channels*4), 
+                UnetAttentionBlock(d_model=d_model*4, 
+                                   num_heads=num_heads),
+            ),
+            # 7'. [B, channels*6, H/32, W/32] => [B, channels*4, H/32, W/32] => [B, channels*4, H/16, W/16]
+            SwitchSequential(
+                UnetResidualBlock(in_channels=channels*6,
+                                  out_channels=channels*4),
+                UnetAttentionBlock(d_model=d_model*4, 
+                                   num_heads=num_heads),
+                Upsample(channels=channels*4),
+            ),
+            # 6'. [B, channels*6, H/16, W/16] => [B, channels*4, H/16, W/16]
+            SwitchSequential(
+                UnetResidualBlock(in_channels=channels*6,
+                                  out_channels=channels*4),
+                UnetAttentionBlock(d_model=d_model*3, 
+                                   num_heads=num_heads),
+            ),
+            # 5'. [B, channels*6, H/16, W/16] => [B, channels*2, H/16, W/16]
+            SwitchSequential(
+                UnetResidualBlock(in_channels=channels*6,
+                                  out_channels=channels*2),
+                UnetAttentionBlock(d_model=d_model*2, 
+                                   num_heads=num_heads),
+            ),
+            # 4'. # [B, channel*3, H/16, W/16] => [B, channels, H/16, W/16] => [B, channels, H/8, W/8]
+            SwitchSequential(
+                UnetResidualBlock(in_channels=channels*3,
+                                  out_channels=channels),
+                UnetAttentionBlock(d_model=d_model,
+                                   num_heads=num_heads),
+                Upsample(channels=channels),
+            ), 
+            # 3'. [B, channels*2, H/8, W/8] => [B, channels, H/8, W/8]
+            SwitchSequential(
+                UnetResidualBlock(in_channels=channels*2,
+                                  out_channels=channels),
+                UnetAttentionBlock(d_model=d_model,
+                                   num_heads=num_heads), 
+            ),
+            # 2'. [B, channels*2, H/8, W/8] => [B, channels, H/8, W/8]
+            SwitchSequential(
+                UnetResidualBlock(in_channels=channels*2,
+                                  out_channels=channels),
+                UnetAttentionBlock(d_model=d_model,
+                                   num_heads=num_heads)
+            ),
+            # 1'. [B, channels*2, H/8, W/8] => [B, channels, H/8, W/8]
+             SwitchSequential(
+                UnetResidualBlock(in_channels=channels*2,
+                                  out_channels=channels),
+                UnetAttentionBlock(d_model=d_model,
+                                   num_heads=num_heads)
+            )
+        ])
 
     def forward(self, x: torch.Tensor, context: torch.Tensor, time: torch.Tensor):
         # x: [B, 4, H/8, W/8]: output of ther VAE encoder
@@ -188,10 +278,23 @@ class Unet(nn.Module):
         for layer in self.encoders:
             x = layer(x, context, time)
             skip_connection.append(x)
-        
+        print("Output of encoder layers: ")
         for i, x in enumerate(skip_connection):
             print(f"{i+1}: {x.shape}")
 
+        # print("Output of the bottle neck: ")
+        # for i, layer in enumerate(self.bottle_neck):
+        #     x = layer(x, context, time)
+        #     print(f"{i+1}: {x.shape}")
+
+        # print("Output of the encoder layers: ")
+        # for i, layer in enumerate(self.decoders):
+        #     x = torch.cat([x, skip_connection.pop()], dim=1)
+        #     x = layer(x, context, time)
+        #     print(f"{i+1}: {x.shape}")
+        #     if(i == 2):
+        #         break
+        # return x
 
 def main():
     batch_size = 1
